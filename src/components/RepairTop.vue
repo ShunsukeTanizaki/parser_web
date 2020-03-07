80747139e5bf1beb598e6962ed4bbbf6ffe7817d
ShunsukeTanizaki <s21valley@gmail.com>
2020-03-08 00:59:56 +0900

    repairTop img 追加

---
+++ src/components/RepairTop.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <v-parallax src="../assets/img04.jpg" height="650">
+      <v-layout
+        column
+        align-center
+        justify-center
+        class="white--text"
+      >
+        <h2 class="white--text mb-6 display-2 text-center">
+          <em style="text-shadow: 0 0 0.3em rgba(0,0,0,1);">Repair</em>
+        </h2>
+          <!-- <em>How to Care</em> -->
+        <div class="subheading mb-4 text-center">
+          <!-- <em>適切なケアと注意を払って十分に長持ちし続ける製品をお客様に提供します。</em> -->
+        </div>
+      </v-layout>
+    </v-parallax>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "RepairTop"
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Repair.vue
+++ src/views/Repair.vue
@@ -1,17 +1,20 @@
 <template>
   <span>
+    <repair-top></repair-top>
     <repair-service></repair-service>
     <repair-price-list></repair-price-list>
   </span>
 </template>
 
 <script>
+import RepairTop from '@/components/RepairTop';
 import RepairService from '@/components/RepairService';
 import RepairPriceList from '@/components/RepairPriceList';
 
 export default {
   name: 'repaire',
   components: {
+    RepairTop,
     RepairService,
     RepairPriceList
   }
